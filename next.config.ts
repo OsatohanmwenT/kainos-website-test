6dbe6985cbf094335d63be0df1d0cf332f988a6c
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2026-05-18 20:33:34 +0100

    remove the info

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const BACKEND_URL =
+  process.env.API_BASE_URL ??
+  "https://kainos-edge-be-production.up.railway.app/api/v1";
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/v1/:path*",
-        destination: `${process.env.API_BASE_URL}/:path*`,
+        destination: `${BACKEND_URL}/:path*`,
       },
     ];
   },
